9d83d4cab4400f684e03d3e2ac059e69f0dc6c40
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-12-05 21:55:36 +0100

    Fix main

--- src/main/java/es/playNcompete/backend/lib/Main.java
+++ src/main/java/es/playNcompete/backend/lib/Main.java
@@ -1,17 +1,7 @@
 package es.playNcompete.backend.lib;
 
-import es.playNcompete.backend.lib.match_participants.MatchParticipant;
-import es.playNcompete.backend.lib.matches.KartRace;
-import es.playNcompete.backend.lib.participants.IParticipant;
-import es.playNcompete.backend.lib.participants.Individual;
-import es.playNcompete.backend.lib.sports.KartRacing;
-import es.playNcompete.backend.lib.tournaments.Tournament;
-
 public class Main {
     public static void main(String[] args) {
-        IParticipant participant = new Individual();
-        KartRace kartRace = new KartRace(new Venue(), new Tournament(new KartRacing()));
-        participant.addMatchParticipant(new MatchParticipant(kartRace, participant));
-        System.out.println(participant.getMatchParticipants());
+
     }
 }
